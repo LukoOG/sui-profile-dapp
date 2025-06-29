8361dad9eb54d31c12bb3dfae8dfc79e57fbdbf3
unknown <emmanueladesipe01@gmail.com>
2025-06-28 20:59:28 -0700

    added wallet connection

---
+++ frontend-nextjs/src/app/components/header.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { ConnectButton } from "@mysten/dapp-kit"
+
+export default function Navbar(){
+    return(
+        <div>
+            <nav>
+
+            </nav>
+
+            <div>
+                <ConnectButton/>
+            </div>
+        </div>
+    )
+}
